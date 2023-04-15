c6ce6df7f1275cd094474f2bb61f749cdf4e8ae0
SabihTahir <Sabihtahir9@gmail.com>
2023-04-15 17:00:07 +0500

    cool

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    scrollRestoration: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
