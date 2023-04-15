9ea71fc461882d4d042e9f901218e938bc612c5c
SabihTahir <Sabihtahir9@gmail.com>
2023-04-15 12:40:20 +0500

    first commit

---
+++ src/constants/DummyModelData.js
@@ -0,0 +1,76 @@
+export const dummyExploreCardData = [
+    {
+      title: "ControlNet-Openpose",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on Scribble images.",
+      route: "ControlNet-Openpose",
+      badge: "SD-1.5",
+      img: "/home/image1.jpg",
+    },
+    {
+      title: "ControlNet-Scribble",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on Canny edges.",
+      route: "ControlNet-Scribble",
+      img: "/home/image2.jpg",
+  
+      badge: "SD-1.4",
+    },
+    {
+      title: "jagilley/controlnet-hed",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary.",
+      route: "jagilley-controlnet-hed",
+      badge: "SD-1.4",
+      img: "/home/image5.jpg",
+    },
+    {
+      title: "ControlNet-Depth",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on Depth estimation.",
+      route: "ControlNet-Depth",
+      badge: "SD-2.0",
+      img: "/home/image6.jpg",
+    },
+    {
+      title: "ControlNet Canny",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary.",
+      route: "jagilley-controlnet-pose",
+      badge: "SD-2.1",
+      img: "/home/canny.jpg",
+    },
+    {
+      title: "ControlNet MLSD",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary.",
+      route: "ControlNet-MLSD",
+      badge: "SD-1.5",
+      img: "/home/msld.jpg",
+    },
+    {
+      title: "stable-diffusion-inpainting",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary.",
+      route: "stable-diffusion-inpainting",
+      badge: "SD-2.1",
+      img: "/home/impainting.jpg",
+    },
+    {
+      title: "Stable-diffusion-2.1",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary.",
+      route: "Stable-diffusion-2-1",
+      badge: "SD-2.1",
+      img: "/home/sd-2.jpg",
+    },
+    {
+      title: "stable-diffusion-img2img",
+      discription:
+        "ControlNet is a neural network structure to control diffusion models by adding extra conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary.",
+      route: "stable-diffusion-img2img",
+      badge: "SD-2.1",
+      img: "/home/imgtoimg.jpg",
+    },
+  ];
+  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,89 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import "./styles/hero-section.css";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Link from "next/link";
+import { dummyExploreCardData } from "@/constants/DummyModelData";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+ const HeroSection = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="py-12 bg-gray-50 sm:py-16 lg:py-[7rem]">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="text-center lg:text-left lg:flex lg:items-center lg:justify-between">
+          <div className="max-w-[32rem] mx-auto text-5xl font-bold text-gray-900 lg:mx-0 position-relative">
+            Deploy & Scale the Fastest Generative AI Models
+          </div>
+          <div className="flex flex-col justify-between items-start">
+            <p className="max-w-lg mx-auto mt-4 text-sm font-medium text-gray-500 lg:mx-0 lg:mt-0">
+              Just use our high performance APIs or bring your own models. We
+              will supercharge and scale them on your cloud or ours.
+            </p>
+            <a
+              href="/model-hub"
+              title=""
+              className="mt-[16px] inline-flex gap-[10px] items-center justify-center px-5 py-3 text-xs font-bold tracking-widest text-gray-900 uppercase transition-all duration-200 bg-transparent border border-gray-900 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:border-gray-900 hover:bg-gray-900 hover:text-white"
+              role="button"
+            >
+              Try Models <AiOutlineArrowRight />
+            </a>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 mt-12 text-center sm:px-16 md:px-0 md:gap-x-8 gap-y-12 md:text-left md:grid-cols-3">
+          {dummyExploreCardData.slice(0, 3).map((cards, i) => {
+            return (
+              <div key={i}>
+                <Image
+                  className="w-full rounded-md h-[376px] object-cover"
+                  src={cards.img}
+                  height={367}
+                  width={100}
+                  alt=""
+                />
+                <div className="flex items-center  mt-7 justify-between">
+                  <p className="text-lg font-bold text-gray-900">
+                    {cards.title}
+                  </p>
+                  <span className="inline-flex items-center  items-center justify-center px-2 pr-3 py-1 text-sm font-semibold leading-[1rem] text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 ">
+                    ⚡0.5s latency
+                  </span>
+                </div>
+                <p className="mt-3 text-sm font-medium setmaxrange text-gray-500 ">
+                  {cards.discription}
+                </p>
+                <div className="mt-6 flex gap-[10px]">
+                  <Link
+                    href={`/${cards.route}`}
+                    title=""
+                    className="inline-flex gap-[10px] items-center justify-center px-5 py-3 text-xs font-bold tracking-widest text-gray-900 uppercase transition-all duration-200 bg-transparent border border-gray-900 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:border-gray-900 hover:bg-gray-900 hover:text-white"
+                    role="button"
+                  >
+                    Try Model <AiOutlineArrowRight />
+                  </Link>
+
+                  <Link
+                    href={`/${cards.route}`}
+                    title=""
+                    className="inline-flex gap-[10px] items-center justify-center px-5 py-3 text-xs font-bold tracking-widest text-gray-900 uppercase transition-all duration-200 bg-transparent border border-gray-900 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:border-gray-900 hover:bg-gray-900 hover:text-white"
+                    role="button"
+                  >
+                    API <AiOutlineArrowRight />
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* <div className="flex w-full items-center justify-center p-[32px]">
+        <button
+          type="button"
+          className="inline-flex items-center  items-center justify-center px-6 py-3 text-sm font-semibold leading-5 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-700"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Explore More
+        </button>
+      </div> */}
+    </section>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default HeroSection;
